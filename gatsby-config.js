7fc55de9b5370d5824aaaee703edca0bef720086
ny.yi <2n.nayeon.yi@daum.net>
2022-11-04 14:29:03 +0900

    fix gatsby-remark-autolink-headers plugin position

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,13 @@ module.exports = {
             },
           },
           {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              isIconAfterHeader: true,
+              elements: [`h1`, `h2`, `h3`]
+            }
+          },
+          {
             resolve: 'gatsby-remark-prismjs',
             options: {
               classPrefix: 'language-',
@@ -87,6 +94,5 @@ module.exports = {
       }
     },
     'gatsby-plugin-sitemap',
-    `gatsby-remark-autolink-headers`,
   ],
 };

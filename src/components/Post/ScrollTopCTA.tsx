6bf81b8b75782a91aeee68fbc7d6908bb35d32a8
ny.yi <2n.nayeon.yi@daum.net>
2022-11-18 14:08:23 +0900

    add behavior: 'smooth' in scrollTo

--- src/components/Post/ScrollTopCTA.tsx
+++ src/components/Post/ScrollTopCTA.tsx
@@ -20,9 +20,7 @@ const TopButton = styled.button`
 
 const ScrollTopCTA = () => {
   const onScrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-    })
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
   }
 
   return (

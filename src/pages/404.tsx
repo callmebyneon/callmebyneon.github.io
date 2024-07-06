b701c8532d4ada018f84fa152862569635a81a30
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-06 23:38:38 +0900

    feat: wrap HTMLBase to apply theme

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { Link } from 'gatsby'
 import GlobalStyle from 'components/Common/GlobalStyle'
+import HTMLBase from 'components/Common/HTMLBase'
 
 const NotFoundPageWrapper = styled.div`
   display: flex;
@@ -38,15 +39,17 @@ const GoToMainButton = styled(Link)`
 
 const NotFoundPage: FunctionComponent = function () {
   return (
-    <NotFoundPageWrapper>
-      <GlobalStyle />
-      <NotFoundText>404</NotFoundText>
-      <NotFoundDescription>
-        찾을 수 없는 페이지입니다. <br />
-        다른 콘텐츠를 보러 가보시겠어요?
-      </NotFoundDescription>
-      <GoToMainButton to="/">메인으로</GoToMainButton>
-    </NotFoundPageWrapper>
+    <HTMLBase>
+      <NotFoundPageWrapper>
+        <GlobalStyle />
+        <NotFoundText>404</NotFoundText>
+        <NotFoundDescription>
+          찾을 수 없는 페이지입니다. <br />
+          다른 콘텐츠를 보러 가보시겠어요?
+        </NotFoundDescription>
+        <GoToMainButton to="/">메인으로</GoToMainButton>
+      </NotFoundPageWrapper>
+    </HTMLBase>
   )
 }
 

0fbcdd06d7abe77f2b18a57a7c9fad2e8c3512e3
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-06 23:36:19 +0900

    rename: theme context file

--- src/hooks/DarkModeContext.tsx
+++ src/hooks/DarkModeContext.tsx
@@ -1,8 +1,11 @@
 import { createContext, ReactNode, useState } from 'react'
 
-const PREFER_SCHEME = window.matchMedia('(prefer-color-scheme: dark)').matches
-const STORAGE = localStorage?.getItem('theme') === 'dark'
-const defaultDarkMode = STORAGE || PREFER_SCHEME
+const PREFER_SCHEME = window.matchMedia('(prefers-color-scheme: dark)').matches
+  ? 'dark'
+  : 'light'
+const STORAGE = localStorage?.getItem('theme')
+const DEFAULT_THEME = STORAGE || PREFER_SCHEME
+const defaultDarkMode = DEFAULT_THEME === 'dark'
 
 export const DarkModeContext = createContext({
   dark: defaultDarkMode,
@@ -22,7 +25,7 @@ export function useDarkMode() {
   const [dark, setDark] = useState<boolean>(defaultDarkMode)
   const toggleMode = () => {
     setDark(prev => !prev)
-    localStorage.setItem('theme', dark ? 'dark' : 'light')
+    localStorage.setItem('theme', dark ? 'light' : 'dark')
   }
   return { dark, toggleMode }
 }
